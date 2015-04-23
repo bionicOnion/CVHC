a87c9009360e364e7eafec74d119dfd6665f8386
bionicOnion <millerrt3@gmail.com>
2015-04-23 16:48:10 -0500

    Adding organization script to repo

---
+++ Data Converter/organize.m
@@ -0,0 +1,56 @@
+function organize()
+
+topLevel = dir;
+
+trainX = [];
+testX = [];
+
+trainY = [];
+testY = [];
+
+writerCount = 0;
+
+for i=1:size(topLevel)
+    if strfind(topLevel(i).name, 'HSF')
+        midLevel = dir(topLevel(i).name);
+        for j=1:size(midLevel)
+            if (j >= 4)
+                fprintf('Processing writer %04d\n', writerCount);
+                writerCount = writerCount + 1;
+            end
+            lowLevel = dir(strcat(topLevel(i).name, '/', midLevel(j).name));
+            testSet = rand > 5/6; % Assign a percentage of the writers to the test set
+            for k=1:size(lowLevel)
+                if strfind(lowLevel(k).name, 'bmp')
+                    filename = strcat(topLevel(i).name, '/', midLevel(j).name, '/', lowLevel(k).name);
+                    imgX = rgb2gray(imread(filename));
+                    imgX = imresize(imgX, 0.25);
+                    imgX = reshape(imgX.',[],1);
+                    imgY = filename(1,length(filename)-7);
+                    if (testSet)
+                        testX = [testX imgX];
+                        testY = [testY imgY];
+                    else
+                        trainX = [trainX imgX];
+                        trainY = [trainY imgY];
+                    end
+                end
+            end
+            if (j >= 4) 
+                fprintf('\tTraining size: %06d; Test size: %06d\n\n', size(trainX,2), size(testX,2));
+            end
+        end
+    end
+end
+
+save('trainX','trainX');
+save('trainY','trainY');
+save('testX','testX');
+save('testY','testY');
+
+percentTrain = size(trainX,2) / size([trainX testX],2);
+percentTest = 1 - percentTrain;
+
+fprintf('%0.3f%% of the data is for training; %0.3f%% is for testing.\n', percentTrain, percentTest);
+
+end
